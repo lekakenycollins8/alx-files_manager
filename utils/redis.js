e097660396ba3cc676017d681645789b985b8d3b
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-08-30 11:08:07 +0300

    created a RedisClient class that connected to redis and had a method to get a value from redis, set a value in redis, and delete a value from redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,32 @@
 // Class that connects to redis client
 // and provides methods to interact with redis
 
-const redis = require('redis');
+import { createClient } from 'redis';
 
 class RedisClient {
     constructor() {
-        this.client = redis.createClient();
+        // create a new redis client instance
+        this.client = createClient();
+
+        // log error if any
+        this.client.on('error', (err) => {
+            console.log(`Error: ${err}`);
+        });
+    }
+    isAlive() {
+        return this.client.connected;
+    }
+    async get(key) {
+        return this.client.get(key);
+    }
+    async set(key, value, duration) {
+        this.client.setex(key, duration, value);
+    }
+    async del(key) {
+        this.client.del(key);
     }
 }
+
+// export the class
+const redisClient = new RedisClient();
+export default redisClient;
