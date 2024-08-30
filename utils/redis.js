85ee7ef95ded6401ddf27531a104cec303fc6825
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-08-30 11:14:46 +0300

    eslint fix

--- utils/redis.js
+++ utils/redis.js
@@ -4,27 +4,31 @@
 import { createClient } from 'redis';
 
 class RedisClient {
-    constructor() {
-        // create a new redis client instance
-        this.client = createClient();
+  constructor() {
+    // create a new redis client instance
+    this.client = createClient();
 
-        // log error if any
-        this.client.on('error', (err) => {
-            console.log(`Error: ${err}`);
-        });
-    }
-    isAlive() {
-        return this.client.connected;
-    }
-    async get(key) {
-        return this.client.get(key);
-    }
-    async set(key, value, duration) {
-        this.client.setex(key, duration, value);
-    }
-    async del(key) {
-        this.client.del(key);
-    }
+    // log error if any
+    this.client.on('error', (err) => {
+      console.log(`Error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.client.get(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 // export the class
